c2523cc6dfef1db7a557b0c8446c50bb2a563eed
Midori Koçak <mtkocak@gmail.com>
2015-05-03 02:02:52 +0200

    event observers working

--- app/code/local/Midori/Conversion/Model/Observer.php
+++ app/code/local/Midori/Conversion/Model/Observer.php
@@ -5,36 +5,8 @@ class Midori_Conversion_Model_Observer
     {
        // $event = $observer->getEvent();
        // $data = $event->getData();
-       // $layout = $observer->getEvent()->getLayout();
-       //$layout = $observer->getEvent()->getLayout();
-       Mage::log(
-                   'logged',
-                   null,
-                   'customer-login.log'
-               );
-       Mage::getModel('customer/session')->setUserLogin(true);
-       
-       // $layout = Mage::app()->getLayout();
-       // $layout->getUpdate()->addUpdate('
-       // reference name="head">
-       //     <block type="core/text" name="myjs">
-       //         <action method="setText"><text><![CDATA[<script type="text/javascript">
-       //         console.log("user logged in")
-       // </script>]]></text></action>
-       //     </block>
-       // </reference>');
-       // $layout->getUpdate()->load();
-       // $layout->generateXml();
 
-       //$update = $observer->getEvent()->getLayout()->getUpdate();
-       //$update->addHandle('midori_conversion');
-        //$layout = $controller->getLayout();
-        // $block = $layout->createBlock('core/text');
-        // $block->setText(
-        // '<script type="text/javascript">
-        //    console.log("user logged in");
-        // </script>'
-        // );
-        // $layout->getBlock('head')->append($block);          
+       Mage::getModel('customer/session')->setUserLogin(true);
+        
     }
 }  
