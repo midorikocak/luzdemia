b825d5573eeccbc782b64d821ab99bda96eee653
Midori Koçak <mtkocak@gmail.com>
2015-05-18 17:46:59 +0200

    checkout bitti

--- js/midori/rich_document.js
+++ js/midori/rich_document.js
@@ -1,8 +1,31 @@
 /**
  * 
+ * Algorithm - Midori Kocak
+ * 
+ * 	1. There are billing and shipping form field and expressions.
+ * 	2. If shipping is hidden, and no value, shipping = billing.
+ * 	3. Extract order data from cart. Each product data is repeated. (Dream Code)
+ * 	4. Append it to expression.
+ * 	5. Finalize Rich Document
+ * 	6. Add the rich document as an hidden input
+ * 	7. Attach the document as html file to email.
+ * 
  */
+	'use strict';
     var app = angular.module('richDocument',[]);
 
     app.controller('RichDocumentController', ['$scope', function($scope) {
-       console.log($scope);
+    	
+    	$scope.billing = {};
+    	$scope.shipping = {};
+          
+        $scope.$watch(function() {return angular.element('#opc-address-form-shipping').hasClass('hidden'); }, function(){
+        	if(angular.element('#opc-address-form-shipping').hasClass('hidden') == true)
+        		{
+        		$scope.shipping = $scope.billing;
+        		}else
+        			{
+        			$scope.shipping = {};
+        			}
+        });
     }]);
