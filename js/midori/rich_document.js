8488ba42fff0f96e5730ce61db2cdbe71426700e
Midori Koçak <mtkocak@gmail.com>
2015-06-01 03:25:43 +0200

    checkout 5

--- js/midori/rich_document.js
+++ js/midori/rich_document.js
@@ -16,7 +16,7 @@
 
     app.controller('RichDocumentController', ['$scope', function($scope) {
     	
-      $scope.currentorder = angular.element('#checkout-review-table').text();
+      $scope.currentorder = angular.element('#checkout-review-table');
       $scope.currentdate = Date();
     	$scope.billing = {};
     	$scope.shipping = {};
