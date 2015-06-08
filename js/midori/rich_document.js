d66ecff851be97c8f972e90c55fa931aed9403d2
Midori Koçak <mtkocak@gmail.com>
2015-06-08 19:21:06 +0200

    algorithm 12

--- js/midori/rich_document.js
+++ js/midori/rich_document.js
@@ -30,36 +30,40 @@
             billingstreet+=billingarray[i];
          }
          
-         angular.element('#billing-address-select').change(function(){
-            if(angular.element('#billing-address-select option:selected').val()==""){
-             console.log('Yeni Fatura Adresi');
-            }else
-            {
-            console.log(angular.element('#billing-address-select option:selected').text());
-            }
-         });
+         var billing = {
+            street1:billingstreet, firstname: billingfirstname
+         };
          
       }else{
          billingText = angular.element('#billing-address-select option:selected').text();
+         
+         var billingarray = billingText.split(',');
+         
+         var billingfirstname = billingarray[0];
+         var billingstreet = "";
+         for(var i=1; i<billingarray.length;i++)
+         {
+            billingstreet+=billingarray[i];
+         }
+         
+         var billing = {
+            street1:billingstreet, firstname: billingfirstname
+         };
+         
          shippingText = angular.element('#shipping-address-select option:selected').text();
          
-         angular.element('#shipping-address-select').change(function(){
-            if(angular.element('#shipping-address-select option:selected').val()==""){
-             console.log('Yeni Kargo Adresi');
-            }else
-            {
-            console.log(angular.element('#shipping-address-select option:selected').text());
-            }
-         });
-
-         angular.element('#billing-address-select').change(function(){
-            if(angular.element('#billing-address-select option:selected').val()==""){
-             console.log('Yeni Fatura Adresi');
-            }else
-            {
-            console.log(angular.element('#billing-address-select option:selected').text());
-            }
-         });
+         var shippingarray = shippingText.split(',');
+         
+         var shippingfirstname = shippingarray[0];
+         var shippingstreet = "";
+         for(var i=1; i<shippingarray.length;i++)
+         {
+            shippingstreet+=shippingarray[i];
+         }
+         
+         var shipping = {
+            street1:shippingstreet, firstname: shippingfirstname
+         };
          
       }
       var result = {shipping:shippingText,billing:billingText};
@@ -82,26 +86,49 @@
       *  billing shipping check
       */
       
+      var registeredAddress = "";
       // Algorithm
       // Check if visible billing registered address
       // if visible add control to checkbox
       if(jQuery('#billing-address-select').length!=0)
       {
          console.log('kayıtlı adres mevcut');
-         getShippingBillingTexts();
+         registeredAddress = getShippingBillingTexts();
       }
       // if changed check if shipping visible
       
       angular.element('#billing\\:use_for_shipping_yes').change(function(){
-         getShippingBillingTexts();
+         registeredAddress = getShippingBillingTexts();
       });
       
+      angular.element('#shipping-address-select').change(function(){
+         if(angular.element('#shipping-address-select option:selected').val()==""){
+                        registeredAddress = "";
+              	$scope.shipping = {};
+         }else
+         {
+         registeredAddress = getShippingBillingTexts();
+         }
+      });
+
+      angular.element('#billing-address-select').change(function(){
+         if(angular.element('#billing-address-select option:selected').val()==""){
+            registeredAddress = "";
+          $scope.billing = {};
+         }else
+         {
+         registeredAddress = getShippingBillingTexts();
+         }
+      });
       // if shipping visible get shipping values from that input
       // else get shipping values from billing input
       
-
-    	$scope.billing = {};
-    	$scope.shipping = {};
+      if(registeredAddress == ""){
+       	$scope.billing = {};
+       	$scope.shipping = {};
+      }else{
+         console.log(registeredAddress);
+      }
           
         $scope.$watch(function() {return angular.element('#opc-address-form-shipping').hasClass('hidden'); }, function(){
         	if(angular.element('#opc-address-form-shipping').hasClass('hidden') == true)
