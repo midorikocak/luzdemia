1c34630ba4adf2754ae81033060f25542f8813e0
Midori Koçak <mtkocak@gmail.com>
2015-06-12 14:02:39 +0200

    revert debug existent

--- app/code/core/Mage/Core/Model/Resource/Db/Abstract.php
+++ app/code/core/Mage/Core/Model/Resource/Db/Abstract.php
@@ -669,7 +669,7 @@ abstract class Mage_Core_Model_Resource_Db_Abstract extends Mage_Core_Model_Reso
 
         if (!empty($existent)) {
             if (count($existent) == 1 ) {
-                $error = Mage::helper('core')->__('%s already exists.', serialize($object));
+                $error = Mage::helper('core')->__('%s already exists.', $existent[0]);
             } else {
                 $error = Mage::helper('core')->__('%s already exist.', implode(', ', $existent));
             }
